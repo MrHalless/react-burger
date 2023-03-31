23bece44d2b2aa97a5794aadda65ab7bdcdcf7b1
v.paskov <v.paskov@arenadata.io>
2023-03-31 12:32:17 +0300

    sprint6/step1

--- src/index.tsx
+++ src/index.tsx
@@ -13,7 +13,7 @@ const root = ReactDOM.createRoot(
 root.render(
   // <React.StrictMode>
   <Provider store={store}>
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router>
       <App />
     </Router>
   </Provider>
